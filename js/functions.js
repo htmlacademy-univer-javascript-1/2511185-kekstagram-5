00e7a00e33abb5b80f7e7101716fef0f1257f630
Konstantin <kburduk@gmail.com>
2024-10-27 23:11:00 +0500

    M. 5.16 complited

--- js/functions.js
+++ js/functions.js
@@ -1,20 +1,19 @@
-// Cheking the length of the string #1
-function checkingStrLen(string, strLength) {
-  if (string.length <= strLength) {
-    return true;
-  } else {
-    return false;
+function workDay(workStart, workEnd, meetStart, meetDuration) {
+  function timeChange(time) {
+    const [hours, minutes] = time.split(':').map(Number);
+    return hours * 60 + minutes;
   }
+
+  const workStartMin = timeChange(workStart);
+  const workEndMin = timeChange(workEnd);
+  const meetStartMin = timeChange(meetStart);
+  const meetEndMin = meetStartMin + meetDuration;
+
+  return meetStartMin >= workStartMin && meetEndMin <= workEndMin;
 }
 
-// Searching for palindromes #2
-function palindromeSearcher(text) {
-  const clearText = text.toLowerCase().replaceAll(' ', '');
-  const reverseText = clearText.split('').reverse().join('');
-
-  if (clearText === reverseText) {
-    return true;
-  } else {
-    return false;
-  }
-}
+console.log(workDay('08:00', '17:30', '14:00', 90));
+console.log(workDay('8:0', '10:0', '8:0', 120));
+console.log(workDay('08:00', '14:30', '14:00', 90));
+console.log(workDay('14:00', '17:30', '08:0', 90));
+console.log(workDay('8:00', '17:30', '08:00', 900));
