f6c0a8bad21030e7e950d36fc1f5a9669bbfe24c
Konstantin <kburduk@gmail.com>
2024-12-08 23:10:50 +0500

    try to fix problem №2

--- js/functions.js
+++ js/functions.js
@@ -12,8 +12,8 @@ function workDay(workStart, workEnd, meetStart, meetDuration) {
   return meetStartMin >= workStartMin && meetEndMin <= workEndMin;
 }
 
-console.log(workDay('08:00', '17:30', '14:00', 90));
-console.log(workDay('8:0', '10:0', '8:0', 120));
-console.log(workDay('08:00', '14:30', '14:00', 90));
-console.log(workDay('14:00', '17:30', '08:0', 90));
-console.log(workDay('8:00', '17:30', '08:00', 900));
+// console.log(workDay('08:00', '17:30', '14:00', 90));
+// console.log(workDay('8:0', '10:0', '8:0', 120));
+// console.log(workDay('08:00', '14:30', '14:00', 90));
+// console.log(workDay('14:00', '17:30', '08:0', 90));
+// console.log(workDay('8:00', '17:30', '08:00', 900));
