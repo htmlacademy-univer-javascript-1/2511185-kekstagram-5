e946ff6d901e9e539acfff05ba82bca50a3c5cce
Keks <keks@htmlacademy.ru>
2024-12-08 21:12:47 +0300

    :heavy_check_mark: Сборка #6

--- 6/js/rendering-thumbnails.js
+++ 6/js/rendering-thumbnails.js
@@ -1,30 +1,26 @@
 import { createPhotosArray } from './data.js';
 
+const pictureTemplateContent = document.querySelector('#picture').content.querySelector('a');
 
-const renderingThumbnails = () => {
-    const pictureTemplateContent = document.querySelector('#picture').content.querySelector('a');
+const fragment = document.createDocumentFragment();
+const createdPictures = createPhotosArray();
 
-    const fragment = document.createDocumentFragment();
-    const createdPictures = createPhotosArray();
+createdPictures.forEach(({url, description, likes, comments}) => {
+    const pictureElement = pictureTemplateContent.cloneNode(true);
 
-    createdPictures.forEach(({url, description, likes, comments}) => {
-        const pictureElement = pictureTemplateContent.cloneNode(true);
+    const thumbnail = pictureElement.querySelector('img');
+    thumbnail.src = url;
+    thumbnail.alt = description;
 
-        const thumbnail = pictureElement.querySelector('img');
-        thumbnail.src = url;
-        thumbnail.alt = description;
+    const pictureLikes = pictureElement.querySelector('.picture__likes');
+    pictureLikes.textContent = likes;
 
-        const pictureLikes = pictureElement.querySelector('.picture__likes');
-        pictureLikes.textContent = likes;
+    const pictureComments = pictureElement.querySelector('.picture__comments');
+    pictureComments.textContent = comments;
 
-        const pictureComments = pictureElement.querySelector('.picture__comments');
-        pictureComments.textContent = comments;
+    fragment.appendChild(pictureElement);
+});
 
-        fragment.appendChild(pictureElement);
-    });
+const picturesContainer = document.querySelector('.pictures');
+picturesContainer.appendChild(fragment);
 
-    const picturesContainer = document.querySelector('.pictures');
-    picturesContainer.appendChild(fragment);
-};
-
-renderingThumbnails();
