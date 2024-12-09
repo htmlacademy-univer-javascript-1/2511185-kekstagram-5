cf17be13220874eaa8acbdc1fe6880a32073c5d1
Konstantin <kburduk@gmail.com>
2024-12-09 16:14:47 +0500

    Try to send 7.15 part №9999

---
+++ js/rendering-thumbnails.js
@@ -0,0 +1,24 @@
+import { createPhotosArray } from './data.js';
+
+const pictureTemplateContent = document.querySelector('#picture').content.querySelector('a');
+const fragment = document.createDocumentFragment();
+const createdPictures = createPhotosArray();
+
+createdPictures.forEach(({url, description, likes, comments}) => {
+  const pictureElement = pictureTemplateContent.cloneNode(true);
+
+  const thumbnail = pictureElement.querySelector('img');
+  thumbnail.src = url;
+  thumbnail.alt = description;
+
+  const pictureLikes = pictureElement.querySelector('.picture__likes');
+  pictureLikes.textContent = likes;
+
+  const pictureComments = pictureElement.querySelector('.picture__comments');
+  pictureComments.textContent = comments;
+
+  fragment.appendChild(pictureElement);
+});
+
+const picturesContainer = document.querySelector('.pictures');
+picturesContainer.appendChild(fragment);
